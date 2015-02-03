7054986b16db998e96314ec04feaadf91718997b
Austin Rich <eatchikinmor@gmail.com>
2015-02-02 22:43:19 -0500

    Uploaded all assets

---
+++ SRNS_Capstone/Capstone.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SRNS_Capstone
+{
+    public partial class Capstone : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
