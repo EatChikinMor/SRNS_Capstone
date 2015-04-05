e7d652c791e25ed7cd4ff9add44e5d3f3f99a8f6
eatchikinmor <eatchikinmor@gmail.com>
2015-04-04 21:58:41 -0400

    Wasn't committed

---
+++ SRNS_Capstone/AvailableCount.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SQLiteDataHelpers.Objects;
+using SQLiteDataHelpers;
+
+namespace SRNS_Capstone
+{
+    public partial class AvailableCount : System.Web.UI.Page
+    {
+        private User _user
+        {
+            get
+            {
+                if (ViewState["userID"] == null)
+                {
+                    ViewState["userID"] = 0;
+                }
+                return (User)ViewState["userID"];
+            }
+            set
+            {
+                ViewState["userID"] = value;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                User user = (User)Session["User"];
+
+                //Remove before release
+                //if ("localhost" == Request.Url.DnsSafeHost)
+                //{
+                //    User a = new User() { ID = 0, FirstName = "Austin", LastName = "Rich", IsAdmin = true, LoginID = "arich", ManagerID = 0 };
+                //    user = a;
+                //}
+                //Remove before release
+
+                if (user != null)
+                {
+                    _user = user;
+                    ((Capstone)Page.Master).showMenuOptions(user.IsAdmin);
+                    buildGridCount();
+                    
+                }
+                else
+                {
+                    Response.Redirect("~/Default.aspx");
+                }
+            }
+        }
+
+        private void buildGridCount()
+        {
+            bool showProvider = true;
+            var dt = new DBConnector().getAvailableLicensesReport(showProvider);
+
+            if (dt.Rows.Count == 0)
+            {
+                lblSoftwareCount.Text = "No Keys Exist";
+                lblSoftwareCount.ForeColor = Color.Red;
+            }
+            else
+            {
+                lblSoftwareCount.Text = dt.Rows.Count.ToString();
+                gridCounts.DataSource = dt;
+                gridCounts.DataBind();
+            }
+        }
+    }
+}
